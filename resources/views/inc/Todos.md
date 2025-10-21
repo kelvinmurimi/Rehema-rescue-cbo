5fc422cf4c1d187ed9eb54ee65129966f32c0c10
kelvinmurimi <murimikelvin229@gmail.com>
2025-10-21 09:51:59 +0300

    [Feature]Add Our History

---
+++ resources/views/inc/Todos.md
@@ -0,0 +1,10 @@
+## Create about us Page.
+## Create Contact Us Page .
+## Create Beneficiares Page $detail View.
+## OUR BENEFICIARIES Page.
+## Our Partners Page.
+## 
+## FAQs Page.
+## Privacy Policy Page.
+## Terms & Conditions Page.
+##
--- routes/frontend.php
+++ routes/frontend.php
@@ -4,3 +4,24 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Additional frontend routes can be added here grouped
+Route::group(['r'], function () {
+    // Example route
+     Route::get('/about-rehema-rescue-cbo', [HomeController::class, 'about'])->name('about');
+     Route::get('/contact-rehema-rescue-cbo', [HomeController::class, 'contact'])->name('contact');
+     //beneficiaries
+     Route::get('beneficiaries-rehema-rescue-cbo', [HomeController::class, 'beneficiaries'])->name('beneficiaries');
+     Route::get('events-rehema-rescue-cbo  ', [HomeController::class, 'events'])->name('events');
+     Route::get('donation-rehema-rescue-cbo', [HomeController::class, 'donation'])->name('donation');
+     Route::get('volunteer-rehema-rescue-cbo', [HomeController::class, 'volunteer'])->name('volunteer');
+     //FAQs
+     Route::get('faqs-rehema-rescue-cbo', [HomeController::class, 'faqs'])->name('faqs');
+     //partners
+     Route::get('partners-rehema-rescue-cbo', [HomeController::class,'partners'])->name('partners');
+     //programs
+     Route::get('programs-rehema-rescue-cbo', [HomeController::class, 'programs'])->name('programs');
+     //privacy
+     Route::get('privacy-policy-rehema-rescue-cbo', [HomeController::class, 'privacy'])->name('privacy');
+     
+}); 
