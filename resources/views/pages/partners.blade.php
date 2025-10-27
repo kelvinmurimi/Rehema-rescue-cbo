a487242922c1abf47ce2f188e16bafbb7275850a
kelvinmurimi <murimikelvin229@gmail.com>
2025-10-27 15:56:32 +0300

    [Feature]Add Collaborators

--- resources/views/pages/partners.blade.php
+++ resources/views/pages/partners.blade.php
@@ -4,59 +4,117 @@
        <div class="container-fluid py-5">
         <div class="container">
             <div class="text-center mx-auto wow fadeIn" data-wow-delay="0.1s" style="max-width: 500px;">
-                <p class="section-title bg-white text-center text-primary px-3">Events</p>
+                <p class="section-title bg-white text-center text-primary px-3">Collaborators</p>
                 <h1 class="display-6 mb-4">Be a Part of a Global Movement</h1>
             </div>
             <div class="row g-4">
                 <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.1s">
-                    <div class="event-item h-100 p-4">
-                        <img class="img-fluid w-100 mb-4" src="{{asset('partners/p15.png')}}" alt="">
-                        <a href="#" class="h3 text-color-primary d-inline-block">Actionchildren</a>
-                        <p>.</p>
-                       
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p15.png')}}" alt="Action Children Logo">
+                        <h5 class="mt-3">Action Children</h5>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.3s">
-                    <div class="event-item h-100 p-4">
-                        <img class="img-fluid w-100 mb-4" src="{{asset('partners/p10.png')}}" alt="">
-                        <a href="#" class="h3 text-color-primary d-inline-block">Awareness Program</a>
-                        <p>.</p>
-
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p10.png')}}" alt="Partner Logo">
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
-                    <div class="event-item h-100 p-4">
-                        <img class="img-fluid w-100 mb-4" src="{{asset('partners/p14.png')}}" alt="">
-                        <a href="#" class="h3 text-color-primary d-inline-block">Health Care Program</a>
-                        <p>.</p>
-
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p14.png')}}" alt="Partner Logo">
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.1s">
-                    <div class="event-item h-100 p-4">
-                        <img class="img-fluid w-100 mb-4" src="{{asset('partners/p12.png')}}" alt="">
-                        <a href="#" class="h3 text-color-primary d-inline-block">Education Program</a>
-                        <p>.</p>
-
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p12.png')}}" alt="Partner Logo">
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.3s">
-                    <div class="event-item h-100 p-4">
-                        <img class="img-fluid w-100 mb-4" src="{{asset('partners/p4.jpg')}}" alt="">
-                        <a href="#" class="h3 text-color-primary d-inline-block">Awareness Program</a>
-                        <p>.</p>
-
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p4.jpg')}}" alt="Partner Logo">
                     </div>
                 </div>
+                
+                {{-- ---- --}}
+                
+                {{-- ---- --}}
                 <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
-                    <div class="event-item h-100 p-4">
-                        <img class="img-fluid w-100 mb-4" src="{{asset('partners/p5.png')}}" alt="">
-                        <a href="#" class="h3 text-color-primary d-inline-block">Health Care Program</a>
-                        <p>.</p>
-
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p7.png')}}" alt="Partner Logo">
+                    </div>
+                </div>
+                {{-- ---- --}}
+                <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p2.jpg')}}" alt="Partner Logo">
+                    </div>
+                </div>
+                {{-- ---- --}}
+                <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p13.png')}}" alt="Partner Logo">
+                    </div>
+                </div>
+                {{-- ---- --}}
+                <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p11.png')}}" alt="Partner Logo">
+                    </div>
+                </div>
+                {{-- ---- --}}
+                <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p9.png')}}" alt="Partner Logo">
+                    </div>
+                </div>
+                {{--  --}}
+                <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p3.jpg')}}" alt="Partner Logo">
+                    </div>
+                </div>
+                {{--  --}}
+                <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p1.png')}}" alt="Partner Logo">
+                    </div>
+                </div>
+                {{--  --}}
+                <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p5.png')}}" alt="Partner Logo">
+                    </div>
+                </div>
+                {{--  --}}
+                <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay="0.5s">
+                    <div class="partner-item h-100">
+                        <img src="{{asset('partners/p6.png')}}" alt="Partner Logo">
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <style>
+        .partner-item {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            background-color: #ffffff;
+            border-radius: .5rem;
+            padding: 2rem;
+            transition: transform 0.3s ease, box-shadow 0.3s ease;
+            border: 1px solid #f0f0f0;
+        }
+        .partner-item:hover {
+            transform: translateY(-5px);
+            box-shadow: 0 8px 20px rgba(0, 0, 0, 0.08);
+        }
+        .partner-item img {
+            max-height: 120px;
+            width: auto;
+            max-width: 100%;
+            object-fit: contain;
+        }
+    </style>
 @endsection
